7d27bed48fc3a912a535a9abac2d8e5fd6c601d2
jourmooney <jourmooney@gmail.com>
2022-05-15 00:37:19 +0900

    Display images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import Post from "./Post";
 
 const BASE_URL = "http://localhost:8000/";
 
@@ -24,7 +25,13 @@ function App() {
         alert(error);
       });
   }, []);
-  return "Hello world";
+  return (
+    <div className="app_posts">
+      {posts.map((post) => (
+        <Post post={post} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
